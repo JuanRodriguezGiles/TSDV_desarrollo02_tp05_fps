73c3a6f664c5033a89b0a76de416f764564b63cb
Juan <juanrodriguezgiles@gmail.com>
2021-05-03 18:19:24 -0300

    Code cleanup

--- TSDV_TP05_JuanRodriguezGiles/Assets/Scripts/Ghost.cs
+++ TSDV_TP05_JuanRodriguezGiles/Assets/Scripts/Ghost.cs
@@ -44,23 +44,15 @@ public class Ghost : MonoBehaviour, GameManager.IEnemy
         moveStart = Time.time;
         stopTime = Random.Range(5, 15);
     }
+    //--------------------------------------------------------------------------------
     void Update()
     {
         switch (currentState)
         {
             case EnemyState.Moving:
                 if (Vector3.Distance(transform.position, targetPos) < 5f)
-                {
-                    targetPos.x = Random.Range(bounds.x - 50, bounds.x + 50);
-                    targetPos.z = Random.Range(bounds.z - 50, bounds.z + 50);
-                    targetPos.y = FindObjectOfType<Terrain>().terrainData
-                        .GetHeight((int)targetPos.x, (int)targetPos.z) + 2;
-                }
-                transform.position = Vector3.MoveTowards(transform.position, targetPos,
-                    moveSpeed * Time.deltaTime);
-                transform.position = new Vector3(transform.position.x, FindObjectOfType<Terrain>().terrainData
-                    .GetHeight((int)transform.position.x, (int)transform.position.z) + 2, transform.position.z);
-                transform.LookAt(targetPos, Vector3.up);
+                    GetNewTargetPos();
+                MoveGhost();
                 if (Time.time > moveStart + stopTime)
                 {
                     currentState = EnemyState.Idle;
@@ -71,19 +63,41 @@ public class Ghost : MonoBehaviour, GameManager.IEnemy
             case EnemyState.ChasingPlayer:
                 break;
             case EnemyState.Idle:
+                Hover();
                 if (Time.time > idleStart + stopTime)
                 {
                     targetPos = transform.position;
                     moveStart = Time.time;
                     currentState = EnemyState.Moving;
                 }
-                Vector3 tempPos = idlePos;
-                tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * 1) * 0.5f;
-                transform.position = tempPos;
                 break;
             default:
                 break;
         }
     }
     //--------------------------------------------------------------------------------
+    void GetNewTargetPos()
+    {
+        targetPos.x = Random.Range(bounds.x - 50, bounds.x + 50);
+        targetPos.z = Random.Range(bounds.z - 50, bounds.z + 50);
+        targetPos.y = FindObjectOfType<Terrain>().terrainData.GetHeight((int)targetPos.x, (int)targetPos.z) + 2;
+    }
+    void MoveGhost()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, FindObjectOfType<Terrain>().terrainData
+            .GetHeight((int)transform.position.x, (int)transform.position.z) + 2, transform.position.z);
+
+        transform.LookAt(targetPos, Vector3.up);
+    }
+    void Hover()
+    {
+        float amplitude = 0.5f;
+        float frequency = 1f;
+        Vector3 tempPos = idlePos;
+
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+        transform.position = tempPos;
+    }
 }
