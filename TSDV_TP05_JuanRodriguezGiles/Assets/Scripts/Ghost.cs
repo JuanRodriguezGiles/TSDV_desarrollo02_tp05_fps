15eb6817712ffec3020dd27eaa89252f024284eb
Juan <juanrodriguezgiles@gmail.com>
2021-05-03 19:26:17 -0300

    Implemented ChasingPlayer state

--- TSDV_TP05_JuanRodriguezGiles/Assets/Scripts/Ghost.cs
+++ TSDV_TP05_JuanRodriguezGiles/Assets/Scripts/Ghost.cs
@@ -14,7 +14,6 @@ public class Ghost : MonoBehaviour, GameManager.IEnemy
 
     }
     //--------------------------------------------------------------------------------
-    public Material[] randomMaterials;
     public int id;
     enum EnemyState
     {
@@ -31,14 +30,17 @@ public class Ghost : MonoBehaviour, GameManager.IEnemy
     float stopTime;
     Vector3 idlePos;
     const float moveSpeed = 5;
-    const float chaseRange = 5;
+    const float chaseRange = 15;
     [SerializeField] Vector3 targetPos;
     [SerializeField] Vector3 bounds;
+    Terrain terrain;
+    public Material[] randomMaterials;
     //--------------------------------------------------------------------------------
     void OnEnable()
     {
         this.gameObject.GetComponent<Renderer>().material = randomMaterials[Random.Range(0, randomMaterials.Length)];
-        bounds = FindObjectOfType<Terrain>().terrainData.bounds.center;
+        terrain = FindObjectOfType<Terrain>();
+        bounds = terrain.terrainData.bounds.center;
         currentState = EnemyState.Moving;
         targetPos = transform.position;
         moveStart = Time.time;
@@ -59,8 +61,17 @@ public class Ghost : MonoBehaviour, GameManager.IEnemy
                     idleStart = Time.time;
                     idlePos = transform.position;
                 }
+                if (IsPlayerClose())
+                    currentState = EnemyState.ChasingPlayer;
                 break;
             case EnemyState.ChasingPlayer:
+                ChasePlayer();
+                if (!IsPlayerClose())
+                {
+                    currentState = EnemyState.Idle;
+                    idleStart = Time.time;
+                    idlePos = transform.position;
+                }
                 break;
             case EnemyState.Idle:
                 Hover();
@@ -70,6 +81,8 @@ public class Ghost : MonoBehaviour, GameManager.IEnemy
                     moveStart = Time.time;
                     currentState = EnemyState.Moving;
                 }
+                if (IsPlayerClose())
+                    currentState = EnemyState.ChasingPlayer;
                 break;
             default:
                 break;
@@ -78,16 +91,17 @@ public class Ghost : MonoBehaviour, GameManager.IEnemy
     //--------------------------------------------------------------------------------
     void GetNewTargetPos()
     {
-        targetPos.x = Random.Range(bounds.x - 50, bounds.x + 50);
-        targetPos.z = Random.Range(bounds.z - 50, bounds.z + 50);
-        targetPos.y = FindObjectOfType<Terrain>().terrainData.GetHeight((int)targetPos.x, (int)targetPos.z) + 2;
+        targetPos.x = Random.Range(bounds.x - 30, bounds.x + 30);
+        targetPos.z = Random.Range(bounds.z - 30, bounds.z + 30);
+        targetPos.y = terrain.terrainData.GetHeight((int)targetPos.x, (int)targetPos.z) + 2;
     }
     void MoveGhost()
     {
         transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
 
-        transform.position = new Vector3(transform.position.x, FindObjectOfType<Terrain>().terrainData
-            .GetHeight((int)transform.position.x, (int)transform.position.z) + 2, transform.position.z);
+        transform.position = new Vector3(transform.position.x,
+            terrain.terrainData.GetHeight((int)transform.position.x, (int)transform.position.z) + 2,
+            transform.position.z);
 
         transform.LookAt(targetPos, Vector3.up);
     }
@@ -99,5 +113,21 @@ public class Ghost : MonoBehaviour, GameManager.IEnemy
 
         tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
         transform.position = tempPos;
+    }
+    bool IsPlayerClose()
+    {
+        return Vector3.Distance(transform.position, FindObjectOfType<CharacterController>().transform.position) <
+               chaseRange;
+    }
+    void ChasePlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position,
+            FindObjectOfType<CharacterController>().transform.position, moveSpeed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x,
+            terrain.terrainData.GetHeight((int)transform.position.x, (int)transform.position.z) + 2,
+            transform.position.z);
+
+        transform.LookAt(FindObjectOfType<CharacterController>().transform.position, Vector3.up);
     }
 }
